fb0004b306eb9d5cf8a2a5a83fa8b75ac2958f80
dowodewe32-droid <dowodewe32@gmail.com>
2026-02-25 19:04:21 +0800

    Update repeater.h

--- repeater.h
+++ repeater.h
@@ -1,10 +1,20 @@
-#pragma once
+#ifndef REPEATER_H
+#define REPEATER_H
 
 #include <Arduino.h>
 
 namespace repeater {
-    void connect(String ssid,String pass);
+    // Fungsi untuk menyimpan kredensial repeater
+    void connect(String ssid, String pass);
+    
+    // Fungsi utama untuk menjalankan mode NAPT/Repeater
     void run();
+    
+    // Mengecek apakah mode repeater aktif
     bool status();
+    
+    // Mengubah status aktif/non-aktif repeater
     void update_status(bool s);
 }
+
+#endif
