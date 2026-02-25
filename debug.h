eb5bed651c498420a70a982d69f6baeff0c6dffc
dowodewe32-droid <dowodewe32@gmail.com>
2026-02-25 21:52:15 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,29 +1,22 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-// 1. Load config dan Arduino di LUAR blok extern "C"
 #include "config.h"
 #include <Arduino.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-    // Kosongkan atau isi hanya dengan fungsi C murni
-#ifdef __cplusplus
-}
-#endif
+// Kita hapus extern "C" karena Serial adalah C++
+// Ini bakal ngilangin error "template with C linkage" selamanya
 
-// 2. Definisikan macro debug dengan benar agar dikenali di .cpp
 #ifdef ENABLE_DEBUG
-    #define debugF(...) DEBUG_PORT.print(__VA_ARGS__)
-    #define debuglnF(...) DEBUG_PORT.println(__VA_ARGS__)
-    #define debug(...) DEBUG_PORT.print(__VA_ARGS__)
-    #define debugln(...) DEBUG_PORT.println(__VA_ARGS__)
+  #define debug(...) DEBUG_PORT.print(__VA_ARGS__)
+  #define debugln(...) DEBUG_PORT.println(__VA_ARGS__)
+  #define debugF(...) DEBUG_PORT.print(__VA_ARGS__)
+  #define debuglnF(...) DEBUG_PORT.println(__VA_ARGS__)
 #else
-    #define debugF(...)
-    #define debuglnF(...)
-    #define debug(...)
-    #define debugln(...)
+  #define debug(...)
+  #define debugln(...)
+  #define debugF(...)
+  #define debuglnF(...)
 #endif
 
 #endif
